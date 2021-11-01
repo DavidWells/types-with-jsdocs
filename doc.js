06549a6ed92d203d7a1db7822f6fd11d27402568
David Wells <>
2021-10-31 21:52:35 -0700

    chore: update examples
    Signed-off-by: David Wells <>

--- doc.js
+++ doc.js
@@ -3,9 +3,15 @@ const path = require('path')
 const dox = require('doxxx')
 
 
-const filePath = path.resolve(__dirname, 'src/1_Button_JSDoc_Simple/index.jsx')
+// const filePath = path.resolve(__dirname, 'src/1_Button_JSDoc_Simple/index.jsx')
+// const filePath = path.resolve(__dirname, 'src/2_Button_JSDoc_TypeDef_Function/index.jsx')
+// const filePath = path.resolve(__dirname, 'src/2.1_Button_JSDoc_Function_Extend/index.jsx')
+// const filePath = path.resolve(__dirname, 'src/3_Button_JSDoc_TypeDef_Class/index.jsx')
+// const filePath = path.resolve(__dirname, 'src/4_Button_JSDoc_ImportedTypes/index.jsx')
+const filePath = path.resolve(__dirname, 'src/5_Button_JSDoc_ImportedTypes_React/index.jsx')
+
 const fileCode = fs.readFileSync(filePath, 'utf-8')
-const obj = dox.parseComments(fileCode);
+const obj = dox.parseComments(fileCode)
 const { inspect } = require('util')
 console.log('result')
 console.log(inspect(obj, {showHidden: false, depth: null}))
--- src/utils/array.js
+++ src/utils/array.js
@@ -1,24 +1,47 @@
 /**
  * JSDoc style
- * @type {Array.<Rgb>}
+ * @typedef {object} RgbColors
+ * @property {number} red
+ * @property {number} green
+ * @property {number} blue
+ */
+
+/**
+ * JSDoc style
+ * @type {Array.<RgbColors>}
  */
 const colors1 = [{ red: 0, green: 0, blue: 0 }];
 
 /**
  * TypeScript style
- * @type {Rgb[]}
- */
-const color2 = [{ red: 111, green: 111, blue: 111 }];
-
-/**
- * TypeScript style
- * @type {Array<Rgb>}
+ * @type {Array<RgbColors>}
  */
 const color3 = [{ red: 255, green: 255, blue: 255 }];
 
+/**
+ * JSDoc type bracket
+ * @type {String[]}
+ */
+const arrayOfStrings = ['a', 'b'];
+
+/**
+ * @type {Number[]}
+ */
+const arrayOfNumbers = [1, 2, 3];
+
+/**
+ * @type {Boolean[]}
+ */
+const arrayOfBools = [true, false, true];
+
+/**
+ * TypeScript style
+ * @type {RgbColors[]}
+ */
+const color2 = [{ red: 111, green: 111, blue: 111 }];
 
 /**
  * TypeScript style array of objects
  * @type Array<{localPath:string, fullPath: string}>
  */
-let filePaths = [];
+let filePaths = [{ localPath: 'xyz', fullPath: 'abc' }];
