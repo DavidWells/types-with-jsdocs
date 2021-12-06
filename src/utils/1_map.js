d2754e039b211a657110acb5fe42094fd7be7eb0
David Wells <>
2021-12-06 12:20:19 -0800

    chore: update map
    Signed-off-by: David Wells <>

--- src/utils/1_map.js
+++ src/utils/1_map.js
@@ -1,7 +1,15 @@
 // https://gist.github.com/hrdtbs/f7e567c0738725a16a9de7326dc11127
 
 /** @type Map.<string, any> */
-/** @type Map<string, any> */
+const elephant = new Map()
 
-/** @type Set.<string> */
-/** @type Set<string> */
+/** @type Map<string, any> */
+const elephantMap = new Map()
+
+/**
+ * The child values of this Type
+ * @since 0.5.0
+ * @type {Map<string, any>}
+ * @private
+ */
+const birdBox = new Map();
